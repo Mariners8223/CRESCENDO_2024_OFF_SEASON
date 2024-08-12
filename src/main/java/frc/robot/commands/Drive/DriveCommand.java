3c2dd88d3885418fd9bc9f195f72c01bdf5b2376
captainsoccer <justeyallevi@gmail.com>
2024-08-12 19:10:17 +0300

    add center of rotation control for turning based on controller POV

--- src/main/java/frc/robot/commands/Drive/DriveCommand.java
+++ src/main/java/frc/robot/commands/Drive/DriveCommand.java
@@ -1,13 +1,28 @@
 package frc.robot.commands.Drive;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
 import frc.robot.subsystems.DriveTrain.DriveBase;
+
+import static frc.robot.subsystems.DriveTrain.SwerveModules.SwerveModule.distanceBetweenWheels;
+
 
 public class DriveCommand extends Command {
 
     private final DriveBase driveBase;
     private final CommandPS5Controller controller;
+
+    private static final Translation2d[] possibleCenterOfRotations = {
+            new Translation2d(distanceBetweenWheels / 2, 0),
+            new Translation2d(distanceBetweenWheels / 2, -distanceBetweenWheels / 2),
+            new Translation2d(0, -distanceBetweenWheels / 2),
+            new Translation2d(-distanceBetweenWheels / 2, -distanceBetweenWheels / 2),
+            new Translation2d(-distanceBetweenWheels /2 , 0),
+            new Translation2d( -distanceBetweenWheels, distanceBetweenWheels / 2),
+            new Translation2d(0, distanceBetweenWheels / 2),
+            new Translation2d(distanceBetweenWheels / 2, distanceBetweenWheels / 2)
+    };
 
     public DriveCommand(DriveBase driveBase, CommandPS5Controller controller) {
         this.driveBase = driveBase;
@@ -18,7 +33,11 @@ public class DriveCommand extends Command {
 
     @Override
     public void initialize() {
-        driveBase.drive(0, 0, 0);
+        driveBase.drive(0, 0, 0, new Translation2d());
+    }
+
+    private static double deadBand(double value) {
+        return Math.abs(value) > 0.1 ? value : 0;
     }
 
 
@@ -27,21 +46,27 @@ public class DriveCommand extends Command {
         //calculates a value from 1 to the max wheel speed based on the R2 axis
         double R2Axis = (1 - (0.5 + controller.getR2Axis() / 2)) * (driveBase.maxFreeWheelSpeed - 1) + 1;
 
+        double povAngle = controller.getHID().getPOV();
+
+        Translation2d centerOfRotation = povAngle == -1 ?
+                new Translation2d() : possibleCenterOfRotations[(int)(povAngle / 45) % 8];
+
         //sets the value of the 3 axis we need (accounting for drift)
-        double leftY = Math.abs(controller.getLeftX()) > 0.1 ? -controller.getLeftX() : 0;
-        double leftX = Math.abs(controller.getLeftY()) > 0.1 ? -controller.getLeftY() : 0;
-        double rightX = Math.abs(controller.getRightX()) > 0.1 ? -controller.getRightX() : 0;
+        double leftX = -deadBand(controller.getLeftX());
+        double leftY = -deadBand(controller.getLeftY());
+        double rightX = deadBand(controller.getRightX());
 
         //drives the robot with the values
         driveBase.drive(
                 leftX * R2Axis,
                 leftY * R2Axis,
-                rightX * R2Axis
+                rightX * R2Axis,
+                centerOfRotation
         );
     }
 
     @Override
     public void end(boolean interrupted) {
-        driveBase.drive(0, 0, 0);
+        driveBase.drive(0, 0, 0, new Translation2d());
     }
 }
