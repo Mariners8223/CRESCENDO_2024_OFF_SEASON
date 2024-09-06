3147d69a9654d5694cbb7811b37806c226620a4f
FrcIroniH <54024395+FrcIroniH@users.noreply.github.com>
2024-09-06 16:24:16 +0300

    add add code

--- src/main/java/frc/robot/commands/ShooterIntake/IntakeFromIntake.java
+++ src/main/java/frc/robot/commands/ShooterIntake/IntakeFromIntake.java
@@ -4,23 +4,71 @@
 
 package frc.robot.commands.ShooterIntake;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.Shooter_Intake.ShooterIntake;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class IntakeFromIntake extends SequentialCommandGroup {
+
+  ShooterIntake shooterIntake;
   /** Creates a new IntakeFromIntake. */
-  public IntakeFromIntake() {
+  public IntakeFromIntake(ShooterIntake shooterIntake) {
     // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
+    // addCommands(new FooCommand(), new BarCommand());\
+
+    this.shooterIntake = shooterIntake;
+
     addCommands(
       new InstantCommand(),
       new ParallelRaceGroup(null)
     );
   }
 
+  private class Step1 extends Command{
+    private Step1(){
 
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+      setGpLoaded();
+    }
+  @Override
+  public boolean isFinished() {
+    return shooterIntake.getBeamBreakValue();
+  }
+
+  }
+
+  private class Step2 extends Command{
+    private Step2(){
+
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+      setGpLoaded();
+    }
+  @Override
+  public boolean isFinished() {
+    return shooterIntake.getBeamBreakValue();
+  }
+
+  }
+  
 }
---
+++ src/main/java/frc/robot/commands/ShooterIntake/ShootShoot.java
@@ -0,0 +1,75 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.ShooterIntake;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.Shooter_Intake.ShooterIntake;
+import frc.robot.subsystems.Shooter_Intake.ShooterIntakeConstants;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class ShootShoot extends SequentialCommandGroup {
+    private static final double INTAKE_SPEED = ShooterIntakeConstants.PresetSpeeds.SPEED4.RPM;
+  /** Creates a new ShootShoot. */
+  public ShootShoot() {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands();
+  }
+   private class Step1 extends Command{
+    private Step1(){
+
+    }
+
+    @Override
+    public void initialize() {
+      shooterIntake.setTargetRPMShooterMotorOffPivot(INPUTED_SPEED);
+      shooterIntake.setTargetRPMShooterMotorOnPivot(INPUTED_SPEED);
+    }
+    public void execute(){
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+  @Override
+  public boolean isFinished() {
+    if (shooterIntake.getTargetRPMShooterMotorOffPivot() && shooterIntake.getTargetRPMShooterMotorOnPivot() == (INPUTED_SPEED)){
+        return true;
+    }
+       return false;
+    
+    //return shooterIntake.getTargetRPMShooterMotorOffPivot();
+    //return shooterIntake.getTargetRPMShooterMotorOnPivot();
+
+  }
+
+  }
+
+  private class Step2 extends Command{
+    private Step2(){
+
+    }
+
+    @Override
+    public void initialize() {
+      shooterIntake.setTargetIntakeMotorRPM(INTAKE_SPEED);
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+    }
+  @Override
+  public boolean isFinished() {
+  return;
+  }
+
+  }
+}
