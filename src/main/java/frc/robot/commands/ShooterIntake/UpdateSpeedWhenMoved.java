b3702c29a246aa7957af6e18f9d5da33c7b513e3
FrcIroniH <54024395+FrcIroniH@users.noreply.github.com>
2024-09-13 11:21:21 +0300

    fixed speedchangedwhenmoved

---
+++ src/main/java/frc/robot/commands/ShooterIntake/UpdateSpeedWhenMoved.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.ShooterIntake;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Shooter_Intake.ShooterIntake;
+
+public class UpdateSpeedWhenMoved extends Command {
+  ShooterIntake shooterIntake;
+  private Supplier<Double> rpmSupplier;
+  /** Creates a new UpdateSpeedWhenMoved. */
+  public UpdateSpeedWhenMoved() {
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  @Override
+  public void execute () {
+    double rpm = rpmSupplier.get();
+
+    shooterIntake.setTargetRPMShooterMotorOffPivot(rpm);
+    shooterIntake.setTargetRPMShooterMotorOnPivot(rpm);
+  }
+
+ @Override
+  public boolean isFinished() {
+    return !shooterIntake.isGpLoaded();
+  }
+}
