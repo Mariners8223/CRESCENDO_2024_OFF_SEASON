ad86847e6f6ead83db548ba603ab01e798547397
coders-code-well <smasherisaac@gmail.com>
2024-09-26 19:21:22 +0300

    Add Beambreaker Threading ~ Asher

--- src/main/java/frc/robot/commands/ShooterIntake/IntakeFromShooter.java
+++ src/main/java/frc/robot/commands/ShooterIntake/IntakeFromShooter.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands.ShooterIntake;
 
+import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
@@ -14,23 +15,18 @@ import frc.robot.subsystems.Shooter_Intake.ShooterIntakeConstants;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class IntakeFromShooter extends SequentialCommandGroup {
+public class IntakeFromShooter extends Command {
   ShooterIntake shooterIntake;
+  Notifier beamBreaker;
+  int counter;
   /** Creates a new IntakeFromShooter. */
   public IntakeFromShooter(ShooterIntake shooterIntake) {
-
     this.shooterIntake = shooterIntake;
-    // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
-
     addRequirements(shooterIntake);
 
-    addCommands(
-      new Step1(),
-      new Step2()
-    );
+    beamBreaker = new Notifier(this::periodic);
+    counter = 0;
   }
-
 
   public static ConditionalCommand getCommand(ShooterIntake shooterIntake){
     ConditionalCommand command = new IntakeFromShooter(shooterIntake).onlyIf(() -> !shooterIntake.isGpLoaded());
@@ -39,52 +35,44 @@ public class IntakeFromShooter extends SequentialCommandGroup {
 
     return command;
   }
-  
 
-  private class Step1 extends Command{
-    @Override
-    public void initialize() {
-      //TODO maybe restart timer?
-      shooterIntake.setTargetShooterMotorOffPivotDutyCycle(ShooterIntakeConstants.Shooter_Speeds.INTAKE_POWER.value);
-      shooterIntake.setTargetShooterMotorOnPivotDutyCycle(ShooterIntakeConstants.Shooter_Speeds.INTAKE_POWER.value);
-      shooterIntake.setIntakeMotorDutyCycle(ShooterIntakeConstants.Intake_Speeds.INTAKE_FROM_SHOOTER_POWER.value);
-    }
-    @Override
-    public void end(boolean interrupted) {
+  private void periodic(){
+    if (counter == 9) return;
+
+    if(shooterIntake.getBeamBreakValue() && counter == 0){
       shooterIntake.stopMotorOffPivot();
       shooterIntake.StopMotorOnPivot();
 
-    }
-
-   @Override
-   public boolean isFinished() {
-    return shooterIntake.getBeamBreakValue();
-    }
-  }
-
-  private class Step2 extends Command{
-    Timer timer = new Timer();
-    int counter;
-    @Override
-    public void initialize() {
-      timer.restart();
-      counter = 0;
-    }
-
-    @Override
-    public void execute(){
       counter++;
     }
 
-    @Override
-    public void end(boolean interrupted) {
-      shooterIntake.setGpLoaded(true);
-      shooterIntake.setIntakeMotorTargetPosition(shooterIntake.getIntakeMotorPositions() - 0.98);
+    else if(counter <= 8) counter++; 
 
+    else{
+      shooterIntake.setIntakeMotorTargetPosition(shooterIntake.getIntakeMotorPositions() - 0.98);
+      counter++;
     }
-   @Override
-    public boolean isFinished() {
-     return counter >= 8;
-    }
+  }
+
+  @Override
+  public void initialize() {
+    //TODO maybe restart timer?
+    shooterIntake.setTargetShooterMotorOffPivotDutyCycle(ShooterIntakeConstants.Shooter_Speeds.INTAKE_POWER.value);
+    shooterIntake.setTargetShooterMotorOnPivotDutyCycle(ShooterIntakeConstants.Shooter_Speeds.INTAKE_POWER.value);
+    shooterIntake.setIntakeMotorDutyCycle(ShooterIntakeConstants.Intake_Speeds.INTAKE_FROM_SHOOTER_POWER.value);
+
+    beamBreaker.startPeriodic(0.01);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    shooterIntake.setGpLoaded(true);
+    beamBreaker.stop();
+    counter = 0;
+  }
+
+  @Override
+  public boolean isFinished() {
+    return counter == 9;
   }
 }
