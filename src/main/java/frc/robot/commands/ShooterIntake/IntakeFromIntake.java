63f2f2a2c4d5eec1402a019a428edec324a6baeb
FrcIroniH <54024395+FrcIroniH@users.noreply.github.com>
2024-10-01 12:33:46 +0300

    made intakeFromIntake run code faster

--- src/main/java/frc/robot/commands/ShooterIntake/IntakeFromIntake.java
+++ src/main/java/frc/robot/commands/ShooterIntake/IntakeFromIntake.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands.ShooterIntake;
 
+import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import frc.robot.subsystems.Shooter_Intake.ShooterIntake;
@@ -14,12 +15,16 @@ import frc.robot.subsystems.Shooter_Intake.ShooterIntakeConstants;
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class IntakeFromIntake extends Command {
   ShooterIntake shooterIntake;
+  boolean isEnded = false;
+  Notifier thread;
   /** Creates a new IntakeFromIntake. */
   private IntakeFromIntake(ShooterIntake shooterIntake) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());\
 
     this.shooterIntake = shooterIntake;
+
+    thread = new Notifier(this::perdioc);
 
     addRequirements(shooterIntake);
   }
@@ -30,18 +35,30 @@ public class IntakeFromIntake extends Command {
 
   @Override
   public void initialize() {
+    isEnded = false;
     shooterIntake.setIntakeMotorDutyCycle(ShooterIntakeConstants.Intake_Speeds.INTAKE_POWER.value);
+    thread.startPeriodic(0.01);
+  }
+
+  private void perdioc(){
+    if(isEnded) return;
+
+    if(shooterIntake.getBeamBreakValue()){
+      isEnded = true;
+      shooterIntake.setIntakeMotorTargetPosition(shooterIntake.getIntakeMotorPositions());
+    }
   }
 
   @Override
   public void end(boolean interrupted) {
-    shooterIntake.setIntakeMotorTargetPosition(shooterIntake.getIntakeMotorPositions() - 1);
+    isEnded = false;
+    thread.stop();
     shooterIntake.setGpLoaded(!interrupted);
   }
 
   @Override
   public boolean isFinished() {
-    return shooterIntake.getBeamBreakValue();
+    return isEnded;
   }
   
 }
