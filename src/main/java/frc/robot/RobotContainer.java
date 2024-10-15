d9bd3cf96b3c716c88b20892aba1ab0e005c7ca0
Gilad Kotzer <gilad.kotzer@gmail.com>
2024-10-15 16:59:24 +0300

    ASHEEEEEEEER :(

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -261,7 +261,7 @@ public class RobotContainer {
         NamedCommands.registerCommand("Shoot", ShootShoot.getCommand(shooterIntake,
                 () -> rpm));
 
-        NamedCommands.registerCommand("Intake", IntakeFromIntake.getCommand(shooterIntake));
+        //NamedCommands.registerCommand("Intake", IntakeFromIntake.getCommand(shooterIntake));
 
         Supplier<Measure<Angle>> supplier = () -> {
             Vision.VisionOutPuts speakerAngle =
@@ -276,14 +276,16 @@ public class RobotContainer {
 
         NamedCommands.registerCommand("Beta Aim", BetaAim.getCommand(arm, supplier));
         NamedCommands.registerCommand("Collect", IntakeFromIntake.getCommand(shooterIntake));
-        NamedCommands.registerCommand("Collect From Floor",
-        new SequentialCommandGroup(MoveArmToPosition.getCommand(arm, ArmConstants.ArmPosition.COLLECT_FLOOR_POSITION),
-                                    IntakeFromIntake.getCommand(shooterIntake)));
+
+        // NamedCommands.registerCommand("Collect From Floor",new SequentialCommandGroup(
+        //     MoveArmToPosition.getCommand(arm, ArmConstants.ArmPosition.COLLECT_FLOOR_POSITION),
+        //     IntakeFromIntake.getCommand(shooterIntake))
+        //     );
         
-        NamedCommands.registerCommand("Shoot to Speaker", new SequentialCommandGroup(
-            BetaAim.getCommand(arm, supplier),
-            ShootShoot.getCommand(shooterIntake, () -> rpm))
-            );
+        // NamedCommands.registerCommand("Shoot to Speaker", new SequentialCommandGroup(
+        //     BetaAim.getCommand(arm, supplier),
+        //     ShootShoot.getCommand(shooterIntake, () -> rpm))
+        //     );
     }
 
 
